47d71737d74c19d39860f6d5c299f200cdb286dd
Mason Jing <y38jing@uwaterloo.ca>
2025-12-07 23:19:31 +0800

    get rid of eslint completely

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// @ts-ignore
-import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +9,6 @@ export default defineConfig({
                 plugins: [["babel-plugin-react-compiler"]],
             },
         }),
-        eslint(),
     ],
     base: "/",
 });
