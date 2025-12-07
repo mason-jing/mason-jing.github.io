3d366deef55606448c0021e586cd0e60da68a4b7
Mason Jing <y38jing@uwaterloo.ca>
2025-12-07 16:11:00 +0800

    update eslint.config.js and README.md

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,45 @@
 import js from "@eslint/js";
-import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-    globalIgnores(["dist"]),
+    ...globalIgnores(["dist"]),
+    ...tseslint.config(
+        js.configs.recommended,
+        tseslint.configs.recommendedTypeChecked,
+        reactX.configs["recommended-typescript"],
+        reactDom.configs.recommended,
+        reactHooks.configs.flat.recommended,
+    ),
+    {
+        files: ["**/*.{js,cjs,mjs}"],
+        extends: [tseslint.configs.disableTypeChecked],
+        rules: {
+            "react-refresh/only-export-components": "off",
+        },
+    },
     {
         files: ["**/*.{ts,tsx}"],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite,
-        ],
         languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
+            parserOptions: {
+                project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
+    },
+    {
+        plugins: {
+            "react-refresh": reactRefresh,
         },
     },
 ]);
